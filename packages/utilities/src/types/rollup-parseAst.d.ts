ffbe75595e4e24e7f1682d2644e0b2b877e2c97a
Ira Hopkinson <irahopkinson@users.noreply.github.com>
2025-11-03 13:00:34 +1300

    utilities: simplify `api-extractor` config (#399)

--- packages/utilities/src/types/rollup-parseAst.d.ts
+++
@@ -1,5 +0,0 @@
-// This was added to silence a warning from `api-extractor`.
-declare module "rollup/parseAst" {
-  export function parseAst(...args: unknown[]): unknown;
-  export function parseAstAsync(...args: unknown[]): unknown;
-}
